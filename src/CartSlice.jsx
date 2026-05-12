2fce95f57870688e3a291ff4837e7e034ffb29b2
Michael Williams <mgwilliams112>
2026-05-12 16:57:24 -0400

    Creating reducers for CartSlice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,21 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const existingItem = state.items.find(item => item.name === action.payload.name);
+        if (existingItem) {
+            existingItem.quantity++
+        } else {
+            state.items.push({...action.payload, quantity: 1})
+        }
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload)
     },
     updateQuantity: (state, action) => {
-
-    
+        const itemToUpdate = state.items.find(item => item.name === action.payload.name);
+        if (itemToUpdate) {
+            itemToUpdate.quantity = action.payload.quantity
+        }
     },
   },
 });
