62698af73868ca622881ca92ad52c54a71b5c93a
Michael Williams <mgwilliams112@gmail.com>
2026-05-14 16:03:44 -0400

    Fixed remove item function

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -15,7 +15,7 @@ export const CartSlice = createSlice({
         }
     },
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload)
+        state.items = state.items.filter(item => item.name !== action.payload.name)
     },
     updateQuantity: (state, action) => {
         const itemToUpdate = state.items.find(item => item.name === action.payload.name);
