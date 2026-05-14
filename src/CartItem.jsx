54160451f0b580273dd244d0f256537257d28d0f
Michael Williams <mgwilliams112@gmail.com>
2026-05-14 10:51:33 -0400

    fixed add to cart functionality, implemented cart count and checkout message

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -20,7 +20,9 @@ const CartItem = ({ onContinueShopping }) => {
         dispatch(onContinueShopping(e))
   };
 
-
+  const handleCheckoutShopping = (e) => {
+        alert('This function will be added for future reference')
+  };
 
   const handleIncrement = (item) => {
         dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}))
@@ -59,7 +61,7 @@ const CartItem = ({ onContinueShopping }) => {
                 <span className="cart-item-quantity-value">{item.quantity}</span>
                 <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
               </div>
-              <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
+              <div className="cart-item-total">Subtotal: ${calculateTotalCost(item)}</div>
               <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
             </div>
           </div>
@@ -69,7 +71,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,188 +3,15 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [
-        {
-            name: "Aloe Vera",
-            image: "https://cdn.pixabay.com/photo/2018/04/02/07/42/leaf-3283175_1280.jpg",
-            description: "Soothing gel used for skin ailments.",
-            cost: 14,
-            quantity: 0
-        },
-        {
-            name: "Echinacea",
-            image: "https://cdn.pixabay.com/photo/2014/12/05/03/53/echinacea-557477_1280.jpg",
-            description: "Boosts immune system, helps fight colds.",
-            cost: 16,
-            quantity: 0
-        },
-        {
-            name: "Peppermint",
-            image: "https://cdn.pixabay.com/photo/2017/07/12/12/23/peppermint-2496773_1280.jpg",
-            description: "Relieves digestive issues and headaches.",
-            cost: 13,
-            quantity: 0
-        },
-        {
-            name: "Lemon Balm",
-            image: "https://cdn.pixabay.com/photo/2019/09/16/07/41/balm-4480134_1280.jpg",
-            description: "Calms nerves and promotes relaxation.",
-            cost: 14,
-            quantity: 0
-        },
-        {
-            name: "Chamomile",
-            image: "https://cdn.pixabay.com/photo/2016/08/19/19/48/flowers-1606041_1280.jpg",
-            description: "Soothes anxiety and promotes sleep.",
-            cost: 15,
-            quantity: 0
-        },
-        {
-            name: "Calendula",
-            image: "https://cdn.pixabay.com/photo/2019/07/15/18/28/flowers-4340127_1280.jpg",
-            description: "Heals wounds and soothes skin irritations.",
-            cost: 12,
-            quantity: 0
-        },
-        {
-            name: "oregano",
-            image: "https://cdn.pixabay.com/photo/2015/05/30/21/20/oregano-790702_1280.jpg",
-            description: "The oregano plants contains compounds that can deter certain insects.",
-            cost: 10,
-            quantity: 0
-        },
-        {
-            name: "Marigold",
-            image: "https://cdn.pixabay.com/photo/2022/02/22/05/45/marigold-7028063_1280.jpg",
-            description: "Natural insect repellent, also adds color to the garden.",
-            cost: 8,
-            quantity: 0
-        },
-        {
-            name: "Geraniums",
-            image: "https://cdn.pixabay.com/photo/2012/04/26/21/51/flowerpot-43270_1280.jpg",
-            description: "Known for their insect-repelling properties while adding a pleasant scent.",
-            cost: 20,
-            quantity: 0
-        },
-        {
-            name: "Basil",
-            image: "https://cdn.pixabay.com/photo/2016/07/24/20/48/tulsi-1539181_1280.jpg",
-            description: "Repels flies and mosquitoes, also used in cooking.",
-            cost: 9,
-            quantity: 0
-        },
-        {
-            name: "Lavender",
-            image: "https://images.unsplash.com/photo-1611909023032-2d6b3134ecba?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            description: "Calming scent, used in aromatherapy.",
-            cost: 20,
-            quantity: 0
-        },
-        {
-            name: "Catnip",
-            image: "https://cdn.pixabay.com/photo/2015/07/02/21/55/cat-829681_1280.jpg",
-            description: "Repels mosquitoes and attracts cats.",
-            cost: 13,
-            quantity: 0
-        },
-        {
-            name: "Lavender",
-            image: "https://images.unsplash.com/photo-1611909023032-2d6b3134ecba?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            description: "Calming scent, used in aromatherapy.",
-            cost: 20,
-            quantity: 0
-        },
-        {
-            name: "Jasmine",
-            image: "https://images.unsplash.com/photo-1592729645009-b96d1e63d14b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            description: "Sweet fragrance, promotes relaxation.",
-            cost: 18,
-            quantity: 0
-        },
-        {
-            name: "Rosemary",
-            image: "https://cdn.pixabay.com/photo/2019/10/11/07/12/rosemary-4541241_1280.jpg",
-            description: "Invigorating scent, often used in cooking.",
-            cost: 15,
-            quantity: 0
-        },
-        {
-            name: "Mint",
-            image: "https://cdn.pixabay.com/photo/2016/01/07/18/16/mint-1126282_1280.jpg",
-            description: "Refreshing aroma, used in teas and cooking.",
-            cost: 12,
-            quantity: 0
-        },
-        {
-            name: "Lemon Balm",
-            image: "https://cdn.pixabay.com/photo/2019/09/16/07/41/balm-4480134_1280.jpg",
-            description: "Citrusy scent, relieves stress and promotes sleep.",
-            cost: 14,
-            quantity: 0
-        },
-        {
-            name: "Hyacinth",
-            image: "https://cdn.pixabay.com/photo/2019/04/07/20/20/hyacinth-4110726_1280.jpg",
-            description: "Hyacinth is a beautiful flowering plant known for its fragrant.",
-            cost: 22,
-            quantity: 0
-        },
-        {
-            name: "Snake Plant",
-            image: "https://cdn.pixabay.com/photo/2021/01/22/06/04/snake-plant-5939187_1280.jpg",
-            description: "Produces oxygen at night, improving air quality.",
-            cost: 15,
-            quantity: 0
-        },
-        {
-            name: "Spider Plant",
-            image: "https://cdn.pixabay.com/photo/2018/07/11/06/47/chlorophytum-3530413_1280.jpg",
-            description: "Filters formaldehyde and xylene from the air.",
-            cost: 12,
-            quantity: 0
-        },
-        {
-            name: "Peace Lily",
-            image: "https://cdn.pixabay.com/photo/2019/06/12/14/14/peace-lilies-4269365_1280.jpg",
-            description: "Removes mold spores and purifies the air.",
-            cost: 18,
-            quantity: 0
-        },
-        {
-            name: "Boston Fern",
-            image: "https://cdn.pixabay.com/photo/2020/04/30/19/52/boston-fern-5114414_1280.jpg",
-            description: "Adds humidity to the air and removes toxins.",
-            cost: 20,
-            quantity: 0
-        },
-        {
-            name: "Rubber Plant",
-            image: "https://cdn.pixabay.com/photo/2020/02/15/11/49/flower-4850729_1280.jpg",
-            description: "Easy to care for and effective at removing toxins.",
-            cost: 17,
-            quantity: 0
-        },
-        {
-            name: "Aloe Vera",
-            image: "https://cdn.pixabay.com/photo/2018/04/02/07/42/leaf-3283175_1280.jpg",
-            description: "Purifies the air and has healing properties for skin.",
-            cost: 14,
-            quantity: 0
-        }
-    ],
+    items: []
   },
   reducers: {
     addItem: (state, action) => {
-        const { name, image, cost } = action.payload; // Destructure product details from the action payload
-        // Check if the item already exists in the cart by comparing names
-        const existingItem = state.items.find(item => item.name === name);
+        const existingItem = state.items.find(item => item.name === action.payload.name);
         if (existingItem) {
-            // If item already exists in the cart, increase its quantity
-            existingItem.quantity++;
+            existingItem.quantity++
         } else {
-            // If item does not exist, add it to the cart with quantity 1
-            state.items.push({ name, image, cost, quantity: 1 });
+            state.items.push({...action.payload, quantity: 1})
         }
     },
     removeItem: (state, action) => {
@@ -196,9 +23,12 @@ export const CartSlice = createSlice({
             itemToUpdate.quantity = action.payload.quantity
         }
     },
+    toggleAdded: (state, action) => {
+        state[action.payload].addedToCart = !state[action.payload].addedToCart
+    }
   },
 });
 
-export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+export const { addItem, removeItem, updateQuantity, toggleAdded } = CartSlice.actions;
 
 export default CartSlice.reducer;
